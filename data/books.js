811ef77a93080c0e465077fe38da13ce1b926ce6
J-Mro <j.k.mroueh@gmail.com>
2026-04-25 22:14:18 +0100

    add hardcoded books data

---
+++ data/books.js
@@ -0,0 +1,46 @@
+const books = [
+  {
+    id: 1,
+    title: "The Pragmatic Programmer",
+    author: "David Thomas",
+    genre: "technology",
+    available: true,
+  },
+  {
+    id: 2,
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    genre: "technology",
+    available: false,
+  },
+  {
+    id: 3,
+    title: "Sapiens",
+    author: "Yuval Noah Harari",
+    genre: "history",
+    available: true,
+  },
+  {
+    id: 4,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "fiction",
+    available: true,
+  },
+  {
+    id: 5,
+    title: "Dune",
+    author: "Frank Herbert",
+    genre: "fiction",
+    available: false,
+  },
+  {
+    id: 6,
+    title: "A Brief History of Time",
+    author: "Stephen Hawking",
+    genre: "science",
+    available: true,
+  },
+];
+
+module.exports = books;
