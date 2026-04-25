6e435ce212a06ae26a6b7028ed94331f9675baaa
J-Mro <j.k.mroueh@gmail.com>
2026-04-25 22:41:30 +0100

    add getBookById method

--- controller/books.controller.js
+++ controller/books.controller.js
@@ -1,9 +1,22 @@
-const { getBooks: getBooksService } = require("../service/books.service");
+const {
+  getBooks: getBooksService,
+  getBookById: getBookByIdService,
+} = require("../service/books.service");
 
 exports.getBooks = async (req, res, next) => {
   try {
     const books = await getBooksService();
     res.status(200).send(books);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getBookById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const book = await getBookByIdService(id);
+    res.status(200).send(book);
   } catch (err) {
     next(err);
   }
