a801ce4b474e1fcbfa129319dcbdc51ee178f20b
J-Mro <j.k.mroueh@gmail.com>
2026-04-25 22:41:59 +0100

    add getBookById method

--- model/books.model.js
+++ model/books.model.js
@@ -3,3 +3,8 @@ const books = require("../data/books");
 exports.getBooks = () => {
   return books;
 };
+
+exports.getBookById = (id) => {
+  const bookById = books.filter((book) => book.id === +id);
+  return bookById[0];
+};
