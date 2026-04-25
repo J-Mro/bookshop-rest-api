104cff9951d21f93589729852c127ef10bfb90ad
J-Mro <j.k.mroueh@gmail.com>
2026-04-25 22:41:03 +0100

    add /:id endpoint

--- routes/books.router.js
+++ routes/books.router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const {
   getBooks: getBooksController,
+  getBookById: getBookByIdController,
 } = require("../controller/books.controller");
 const router = express.Router();
 
 router.route("/").get(getBooksController);
+router.route("/:id").get(getBookByIdController);
 module.exports = router;
