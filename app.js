1c1268263df9462e50230936272025539b3d42ef
J-Mro <j.k.mroueh@gmail.com>
2026-04-25 23:12:07 +0100

    add error handling middleware with first case being instanceof NotFoundError

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const booksRouter = require("./routes/books.router");
+const NotFoundError = require("./errors/NotFoundError");
 const app = express();
 
 app.use(express.json());
@@ -8,6 +9,14 @@ app.all("/*path", (req, res, next) => {
   res
     .status(404)
     .send({ msg: "The path you're looking for does not exist! </3" });
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof NotFoundError) {
+    res.status(404).send({ msg: "404: " + err.message });
+  } else {
+    next(err);
+  }
 });
 
 module.exports = app;
