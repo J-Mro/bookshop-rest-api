e825b5622fd2e19fe812bb770f00e3db1c65f90b
J-Mro <j.k.mroueh@gmail.com>
2026-04-25 23:11:13 +0100

    add error handling for invalid book id

--- service/books.service.js
+++ service/books.service.js
@@ -2,10 +2,17 @@ const {
   getBooks: getBooksModel,
   getBookById: getBookByIdModel,
 } = require("../model/books.model");
+const { checkBookExists } = require("../utils/checkBookExists");
+const NotFoundError = require("../errors/NotFoundError");
 exports.getBooks = () => {
   return getBooksModel();
 };
 
-exports.getBookById = (id) => {
-  return getBookByIdModel(id);
+exports.getBookById = async (id) => {
+  const bookExists = await checkBookExists(id);
+  if (!bookExists) {
+    throw new NotFoundError("The book you're looking for was not found");
+  } else {
+    return getBookByIdModel(id);
+  }
 };
