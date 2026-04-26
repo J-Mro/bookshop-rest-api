ce15555dfe5dbe25f8a0ef2fb3bca2ee8896980c
J-Mro <j.k.mroueh@gmail.com>
2026-04-26 17:10:25 +0100

    add post method chained on / route

--- routes/books.router.js
+++ routes/books.router.js
@@ -2,9 +2,10 @@ const express = require("express");
 const {
   getBooks: getBooksController,
   getBookById: getBookByIdController,
+  postBook: postBookController,
 } = require("../controller/books.controller");
 const router = express.Router();
 
-router.route("/").get(getBooksController);
+router.route("/").get(getBooksController).post(postBookController);
 router.route("/:id").get(getBookByIdController);
 module.exports = router;
