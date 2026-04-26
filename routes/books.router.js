9e7806ca089330d6f1e1e726f2744efe2e23dbcd
J-Mro <j.k.mroueh@gmail.com>
2026-04-26 20:13:36 +0100

    add patch method chained on '/:id' route

--- routes/books.router.js
+++ routes/books.router.js
@@ -3,9 +3,10 @@ const {
   getBooks: getBooksController,
   getBookById: getBookByIdController,
   postBook: postBookController,
+  patchBook: patchBookController,
 } = require("../controller/books.controller");
 const router = express.Router();
 
 router.route("/").get(getBooksController).post(postBookController);
-router.route("/:id").get(getBookByIdController);
+router.route("/:id").get(getBookByIdController).patch(patchBookController);
 module.exports = router;
